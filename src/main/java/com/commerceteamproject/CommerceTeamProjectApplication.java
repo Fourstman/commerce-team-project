e97c2882667c52ea160e6241c7815c15e65a8d22
곽현민 <hunmin111@gmail.com>
2026-01-13 20:33:04 +0900

    init

---
+++ src/main/java/com/commerceteamproject/CommerceTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.commerceteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceTeamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/commerceteamproject/CommerceTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerceteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
