a639e00b0658f72d701bf89e12d3e6391d673a03
DEV.HYUCK <wwh3218@gmail.com>
2026-01-14 19:04:51 +0900

    fix: 클래스명 오타 수정

--- src/main/java/com/commerceteamproject/product/controller/ProductController.java
+++ src/main/java/com/commerceteamproject/product/controller/ProductController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class ProductContoroller {
+public class ProductController {
 
     private final ProductService productService;
 
