5c73fe195f275183dcbed6641217b99c93d389d3
곽현민 <hunmin111@gmail.com>
2026-01-18 10:44:23 +0900

    fix: 주문 생성 요청 검증 수정

--- src/main/java/com/commerceteamproject/order/dto/CreateOrderRequest.java
+++ src/main/java/com/commerceteamproject/order/dto/CreateOrderRequest.java
@@ -1,15 +1,15 @@
 package com.commerceteamproject.order.dto;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class CreateOrderRequest {
-    @NotBlank
+    @NotNull
     private Long customerId;
-    @NotBlank
+    @NotNull
     private Long productId;
-    @NotBlank @Min(value = 1, message = "수량은 1 이상이어야 합니다.")
+    @NotNull @Min(value = 1, message = "수량은 1 이상이어야 합니다.")
     private int quantity;
 }
