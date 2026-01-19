1dfed5297de453bf59766ae76fca071d95f20517
perfect-Bee <joonslee3@gmail.com>
2026-01-19 12:37:06 +0900

    fix : 거부사유 예외처리 수정

--- src/main/java/com/commerceteamproject/admin/dto/AdminRejectRequest.java
+++ src/main/java/com/commerceteamproject/admin/dto/AdminRejectRequest.java
@@ -1,5 +1,6 @@
 package com.commerceteamproject.admin.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,5 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AdminRejectRequest {
     // 거부 사유도 적어야해서 거절사유
+    @NotBlank(message = "거부 사유는 반드시 필요합니다.")
     private String reason;
 }
